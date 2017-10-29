9c3d39881e574cad78492d4fe39be67486990ac8
kdcrafter <kurtisdoubled@hotmail.com>
2017-10-28 22:54:09 -0500

    added basic gui layout

--- src/Interface.java
+++ src/Interface.java
@@ -1,13 +1,38 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class Interface {
+    //the interface itself
+    JFrame frame;
+
+    //components of the interface
+    JButton clear;
+    JButton save;
+    JPanel drawing;
+
     public Interface() {
         //create the GUI
-        JFrame frame = new JFrame("Paint ASCII");
+        this.frame = new JFrame("Paint ASCII");
 
         //edit the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //full screen
-        frame.setVisible(true);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //full screen
+        this.frame.setVisible(true);
+
+        //set up the top part of the interface
+        this.clear = new JButton("Clear");
+        this.clear.setPreferredSize(new Dimension(this.frame.getWidth()/2, this.frame.getHeight()/10));
+        this.frame.getContentPane().add(this.clear, BorderLayout.LINE_START);
+
+        this.save = new JButton("Save");
+        this.save.setPreferredSize(new Dimension(this.frame.getWidth()/2, this.frame.getHeight()/10));
+        this.frame.getContentPane().add(this.save, BorderLayout.LINE_END);
+
+        this.drawing = new JPanel();
+        this.drawing.setPreferredSize(new Dimension(this.frame.getWidth(), (int) (this.frame.getHeight() * 0.9)));
+        this.frame.getContentPane().add(this.drawing, BorderLayout.SOUTH);
+
+        this.frame.pack();
+        this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //full screen
     }
 }
