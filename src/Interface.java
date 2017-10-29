642ff373491ecc8406478d9c6e3a0a4928adbed5
kdcrafter <kurtisdoubled@hotmail.com>
2017-10-28 20:22:56 -0500

    added basic Jframe gui

--- src/Interface.java
+++ src/Interface.java
@@ -1,2 +1,13 @@
+import javax.swing.JFrame;
+
 public class Interface {
+    public Interface() {
+        //create the GUI
+        JFrame frame = new JFrame("Paint ASCII");
+
+        //edit the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //full screen
+        frame.setVisible(true);
+    }
 }
