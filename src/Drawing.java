85cdf04097176a126c95e4a4cfe7c8495bf53e58
kdcrafter <kurtisdoubled@hotmail.com>
2017-11-18 22:40:11 -0600

    created GUI on EDT and added drawing basics

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,2 +1,15 @@
-public class Drawing {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Drawing extends JPanel {
+    Image img = null; //the thing that will be drawn on
+    Graphics2D draw;  //used to draw lines
+
+    //the current and previous coordinates of the mouse
+    int currX, currY, prevX, prevY;
+
+    public Drawing() {
+
+    }
 }
