43092cda188de8122f5538908dc180ad063a01d9
kdcrafter <kurtisdoubled@hotmail.com>
2017-11-19 23:25:06 -0600

    completed simple interface

--- src/Interface.java
+++ src/Interface.java
@@ -6,7 +6,7 @@ public class Interface {
     private JFrame frame;
 
     //the left side of the interface
-    private JTextField drawing; //will eventually need to be type Drawing
+    private JPanel drawing; //will eventually need to be type Drawing
     //JButton clear;
 
     //the center components of the interface
@@ -15,13 +15,8 @@ public class Interface {
     //the right side of the interface
     private JTextField resultText;
 
-    //other stuff in the class
-    private GridBagConstraints gbc;
-
 
     public Interface() {
-        gbc = new GridBagConstraints();
-
         createInterface();
     }
 
@@ -32,57 +27,28 @@ public class Interface {
 
         //edit the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);  //full screen
-        frame.setLayout(new GridBagLayout());
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        frame.setSize(1600, 700);
+        frame.setResizable(false);
+        BorderLayout layout = new BorderLayout();
+        layout.setVgap(10);
+        layout.setHgap(10);
+        frame.setLayout(layout);
 
-        //add the components
-        drawing = new JTextField();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridy = 0;
-        gbc.gridx = 0;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.weightx = 0.5;
-        gbc.weighty = 0;
-        gbc.anchor = GridBagConstraints.LAST_LINE_END;
-        gbc.insets = new Insets(0, 50, 50, 50);
-        frame.add(drawing, gbc);
+        //add components
+        drawing = new JPanel();
+        drawing.setPreferredSize(new Dimension(700, 700));
+        frame.add(drawing, BorderLayout.LINE_START);
 
-        toText = new JButton();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridy = 2;
-        gbc.gridx = 2;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.weightx = 0.5;
-        gbc.weighty = 0;
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.insets = new Insets(10, 10, 10, 10);
-        frame.add(toText, gbc);
+        toText = new JButton("To Text ->");
+        toText.setPreferredSize(new Dimension(100, 100));
+        frame.add(toText, BorderLayout.CENTER);
 
         resultText = new JTextField();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridy = 4;
-        gbc.gridx = 4;
-        gbc.gridwidth = 2;
-        gbc.gridheight = 1;
-        gbc.weightx = 0.5;
-        gbc.weighty = 0;
-        gbc.anchor = GridBagConstraints.LAST_LINE_START;
-        gbc.insets = new Insets(0, 50, 50, 50);
-        frame.add(resultText, gbc);
+        resultText.setPreferredSize(new Dimension(700, 700));
+        frame.add(resultText, BorderLayout.LINE_END);
 
         //show the interface
         frame.pack();
         frame.setVisible(true);
-    }
-
-    //sets the properties of the components
-    private void setGridBagConstraints(int y, int x, int width, int height) {
-        gbc.gridy = y;
-        gbc.gridx = x;
-        gbc.gridwidth = width;
-        gbc.gridheight = height;
     }
 }
