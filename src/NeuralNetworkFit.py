674a21df6f057212352ead29c0f7af301821b694
kdcrafter <kurtisdoubled@hotmail.com>
2018-01-03 00:25:32 -0600

    added functions to read and store data from the MNIST files

--- src/NeuralNetworkFit.py
+++ src/NeuralNetworkFit.py
@@ -1,4 +1,7 @@
 from sklearn.neural_network import MLPClassifier
+import os
+import struct
+import numpy as np
 
 class NeuralNetworkFit():
     """
@@ -11,28 +14,69 @@ class NeuralNetworkFit():
     """
 
     def __init__(self):
+        #constants
+        self.PATH = "../Data/"  #the directory the data is in
+        self.FILENAME = "Weights.txt" #the file to store the weights in
+
         #get the data
-        self.X, self.Y = self.getData()
+        #TODO: see about making the inputs and outputs generators instead of iterators
+        self.trainInputs, self.trainOutputs = self.getTrainData()
+        self.testInputs, self.testOutputs = self.getTestData()
 
         #fit the data
         self.clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(1000, 500, 100), random_state=1)
-        self.clf.fit(self.X, self.Y)
+        #self.clf.fit(self.inputs, self.outputs)
 
         #write the data
-        self.FILENAME = "Weights.txt"
         self.writeWeights()
 
-    def getData(self):
+    def getTrainData(self):
         """
-        get data from the MNIST dataset in the form of numpy arrays
+        get the training data from the MNIST dataset in the form of numpy arrays
 
-        :return: the inputs(X) and the outputs(y) of the neural network as a numpy array
+        :return: the inputs and the outputs of the neural network as a numpy array
         """
 
-        X = None
-        Y = None
+        #go to the path of the MNIST files
+        trainImageFileName = os.path.join(self.PATH, 'train-images-idx3-ubyte')
+        trainLabelFileName = os.path.join(self.PATH, 'train-labels-idx1-ubyte')
 
-        return X, Y
+        #store file info into numpy arrays
+        trainLabelFile = open(trainLabelFileName, 'rb')
+        trainLabelFile.seek(8) #skip unessesary info
+        outputs = np.fromfile(trainLabelFile, dtype=np.int8) #store as int8's as values are from 0-255
+        trainLabelFile.close()
+
+        trainImageFile = open(trainImageFileName, 'rb')
+        trainImageFile.seek(16) #skip unessesary info
+        inputs = np.fromfile(trainImageFile, dtype=np.uint8).reshape(len(outputs), 28, 28) #store as int8's as values are from 0-255
+        trainImageFile.close()
+
+        return inputs, outputs
+
+    def getTestData(self):
+        """
+        get the testing data from the MNIST dataset in the form of numpy arrays
+
+        :return: the inputs and the outputs of the neural network as a numpy array
+        """
+
+        # go to the path of the MNIST files
+        testImageFileName = os.path.join(self.PATH, 't10k-images-idx3-ubyte')
+        testLabelFileName = os.path.join(self.PATH, 't10k-labels-idx1-ubyte')
+
+        #store file info into numpy arrays
+        testLabelFile = open(testLabelFileName, 'rb')
+        testLabelFile.seek(8) #skip unessesary info
+        outputs = np.fromfile(testLabelFile, dtype=np.int8) #store as int8's as values are from 0-255
+        testLabelFile.close()
+
+        testImageFile = open(testImageFileName, 'rb')
+        testImageFile.seek(16) #skip unessesary info
+        inputs = np.fromfile(testImageFile, dtype=np.uint8).reshape(len(outputs), 28, 28) #store as int8's as values are from 0-255
+        testImageFile.close()
+
+        return inputs, outputs
 
     def writeWeights(self):
         """
@@ -41,6 +85,13 @@ class NeuralNetworkFit():
         :return: nothing
         """
 
+        # get weights
+        #weights = self.clf.coefs_
 
+        # format weights
+
+        #open file
+
+        #write weights to file
 
 NeuralNetworkFit()  #do the init command
